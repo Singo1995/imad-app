1ac2339fa003ba923286848c88a7186e0e629fc4
Singo1995 <30798718+Singo1995@users.noreply.github.com>
2017-08-12 12:16:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,13 +3,62 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var articleone = {
+    title: "Article One | Surender Singh",
+    heading: "Article One",
+    date: "July 12th 2017",
+    content:
+            `<p>
+                    This is Singo!!!!.I am the Actor of Ennai Nokki Paayum Thotta.The stills which I have posed in this Movie is going viral and Hopefully the first day collection would be above 100 crore
+                    </p>
+                    <p>
+                    This is Singo!!!!.I am the Actor of Ennai Nokki Paayum Thotta.The stills which I have posed in this Movie is going viral and Hopefully the first day collection would be above 100 crore
+                    </p>
+                    <p>
+                    This is Singo!!!!.I am the Actor of Ennai Nokki Paayum Thotta.The stills which I have posed in this Movie is going viral and Hopefully the first day collection would be above 100 crore
+                    </p>`
+};
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate =
+`<html>
+    <head>
+        <title> 
+        ${title} 
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+         </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href ='/'>Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading} 
+                </h3>
+                <div>
+                    ${date} 
+                </div>
+                <div>
+                    ${content} 
+                </div>
+            </div>
+        </body>
+   </html>`;
+   return htmlTemplate;
+}
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
