154140954ef2b494dd21b7b6dac295f5ff2d2ce5
Singo1995 <30798718+Singo1995@users.noreply.github.com>
2017-08-14 12:46:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,9 +79,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-/*app.get('/ui/favicon.ico', function (req, res) {
+app.get('/ui/favicon.ico', function (req, res) {
 res.sendFile(path.join(__dirnam, 'ui','favicon.ico'));
-});*/
+});
 //app.get('/favicon.ico', function (req, res) {
 //res.sendFile(path.join(__dirnam, 'ui','favicon.ico'));
 //});
