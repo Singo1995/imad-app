707c6ee1e23a3e7e733d8e8011fbbb6d0ba6939f
Singo1995 <30798718+Singo1995@users.noreply.github.com>
2017-08-16 12:25:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,12 @@
-console.log('Loaded!');
-//var element = document.getElementById('main-text');
-//element.innerHTML = "This is Theriiiii....";
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-     marginLeft += 5;
-     img.style.marginLeft = marginLeft + 'px';
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    //Request to /counter
     
-}
-img.onclick = function()
-{
-    var interval = setInterval(moveRight,100);
+    //get the response and store it in a variable
     
+    //Render the variable in the correct span
+    counter+=1;
+    var span = document.getElementById('count');
+    span.innerHTML= counter.toString();
 };
