1530743b56770a278119d9b26fff492fd12e1f29
Singo1995 <30798718+Singo1995@users.noreply.github.com>
2017-08-16 10:56:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = "This is Theriiiii....";
+//var element = document.getElementById('main-text');
+//element.innerHTML = "This is Theriiiii....";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
