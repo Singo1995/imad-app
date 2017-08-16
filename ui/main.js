21afbe0c8bea88c2cbc9486b8bb60ecc5ab3d77f
Singo1995 <30798718+Singo1995@users.noreply.github.com>
2017-08-16 16:29:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,52 +1,54 @@
 var button = document.getElementById('counter');
 var counter = 0;
-button.onclick = function(){
+button.onclick = function()
+{
     //create a request object
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-    if (request.readyState === XMLHttpRequest.DONE)
+    request.onreadystatechange = function()
     {
-        if(request.status===200)
+        if (request.readyState === XMLHttpRequest.DONE)
         {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML= counter.toString();
+                if(request.status===200)
+                {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML= counter.toString();
+                }
         }
-    }
     }
     
     //make the request
     request.open('GET', 'http://surendersingh1995nov.imad.hasura-app.io/counter', true);
     request.send();
-    
-   };
+};
    //get name
    
    var submit = document.getElementById('submit_btn');
-   submit.onclick = function(){
+   submit.onclick = function()
+{
     //create a request object
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE)
+    request.onreadystatechange = function()
         {
-            if(request.status===200)
-            {
-               var names = request.responseText;
-               names = JSON.parse(names);
-               for(i = 0;i < name.length;i++){
-               list += '<li>'+ names[i] + '</li>';
-            }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML= list;
-        }
-    }
-    
+                if (request.readyState === XMLHttpRequest.DONE)
+                {
+                        if(request.status===200)
+                        {
+                           var names = request.responseText;
+                           names = JSON.parse(names);
+                               for(i = 0;i < name.length;i++)
+                               {
+                               list += '<li>'+ names[i] + '</li>';
+                               }
+                        var ul = document.getElementById('namelist');
+                        ul.innerHTML= list;
+                        }
+                }
+        }        
     //make the request
     request.open('GET', 'http://surendersingh1995nov.imad.hasura-app.io/submit-name/name='+ name, true);
     request.send();
    //capture the response and convert into html
-   
-  
   };
